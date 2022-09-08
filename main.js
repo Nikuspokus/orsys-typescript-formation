de4221a31aa86622d6f0b507fa4e116126e36323
nmarquillier <nmarquillier@norauto.com>
2022-09-08 15:36:18 +0200

    dev: make multiple line

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 console.log("start");
 
-const samples = 10;
+const computeAngle = (i, samples, multiplicationFactor) => {
+  if (multiplicationFactor === undefined) {
+    multiplicationFactor = 1;
+  }
+  return i * ((2 * Math.PI) / samples) * multiplicationFactor - Math.PI / 2;
+};
+const computePointCoordinate = (angle) => {
+  const x = x0 + r0 * Math.cos(angle);
+  const y = y0 + r0 * Math.sin(angle);
+  return { x, y };
+};
+const samples = 100;
 const multiplicationFactor = 2;
 
 const svgns = "http://www.w3.org/2000/svg";
@@ -29,22 +40,25 @@ for (let i = 0; i < samples; i++) {
   container.appendChild(circle);
 }
 
-const drawLine = (x1, y1, x2, y2) => {
+const drawLine = (p1, p2) => {
   const line = document.createElementNS(svgns, "line");
-  line.setAttributeNS(null, "x1", x1);
-  line.setAttributeNS(null, "y1", y1);
-  line.setAttributeNS(null, "x2", x2);
-  line.setAttributeNS(null, "y2", y2);
+  line.setAttributeNS(null, "x1", p1.x);
+  line.setAttributeNS(null, "y1", p1.y);
+  line.setAttributeNS(null, "x2", p2.x);
+  line.setAttributeNS(null, "y2", p2.y);
   lineContainer.appendChild(line);
 };
 
 for (let i = 0; i < samples; i++) {
-  const angle1 = i * ((2 * Math.PI) / samples) - Math.PI / 2;
-  const angle2 =
-    i * ((2 * Math.PI) / samples) * multiplicationFactor - Math.PI / 2;
-  const x1 = x0 + r0 * Math.cos(angle1);
-  const y1 = y0 + r0 * Math.sin(angle1);
-  const x2 = x0 + r0 * Math.cos(angle2);
-  const y2 = y0 + r0 * Math.sin(angle2);
-  drawLine(x1, y1, x2, y2);
+  //draw a line
+  const angle1 = computeAngle(i, samples);
+  // compute point1 coordinate
+  const p1 = computePointCoordinate(angle1);
+
+  // compute angle2
+  const angle2 = computeAngle(i, samples, multiplicationFactor);
+  // compute point2 coordinate
+  const p2 = computePointCoordinate(angle2);
+
+  drawLine(p1, p2);
 }
