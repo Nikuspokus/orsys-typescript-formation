0006c6669330cc741631b23b75a5691a07141c75
nmarquillier <nmarquillier@norauto.com>
2022-09-09 14:47:10 +0200

    dev: update the board when updating command

--- src/Command.ts
+++ src/Command.ts
@@ -1,16 +1,39 @@
 import { BoardConfig } from "./interfaces/BoardConfig";
 import { querySelector } from "./utils";
 
+type CommandCallBack = (newConfig: BoardConfig) => void;
+
 export class Command {
   config: BoardConfig;
-  onUpdate(arg0: () => void) {
-    throw new Error("Method not implemented.");
+  callback: CommandCallBack;
+
+  constructor() {
+    this.addListeners();
   }
+
+  addListeners() {
+    const boardConfigKeys = ["samples", "multiplicationFactor"];
+    for (const key of boardConfigKeys) {
+      const input = querySelector(
+        `div.command label.${key} input`,
+        HTMLInputElement
+      );
+      input.addEventListener("input", (event) => {
+        this.config[key] = input.value;
+        this.render();
+        this.callback(this.config);
+      });
+    }
+  }
+
+  onUpdate(callback: CommandCallBack) {
+    this.callback = callback;
+  }
+
   render() {
     const boardConfigKeys = ["samples", "multiplicationFactor"];
     for (const key of boardConfigKeys) {
       const elt = querySelector(`div.command label.${key} span`);
-      console.log("elt: ", elt);
       elt.innerHTML = this.config[key].toString();
 
       const sliderElt = querySelector(`div.command label.${key} input`);
@@ -20,8 +43,10 @@ export class Command {
       sliderElt.value = this.config[key].toString();
     }
   }
+
   setConfig(config: BoardConfig) {
     this.config = config;
     this.render();
+    this.callback?.(this.config);
   }
 }
