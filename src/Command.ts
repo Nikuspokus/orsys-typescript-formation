26d4e7d98786aa3502cca9291c8b5cc07af660b3
nmarquillier <nmarquillier@norauto.com>
2022-09-09 14:18:20 +0200

    dev: render slider elt

--- src/Command.ts
+++ src/Command.ts
@@ -1,10 +1,27 @@
 import { BoardConfig } from "./interfaces/BoardConfig";
+import { querySelector } from "./utils";
 
 export class Command {
+  config: BoardConfig;
   onUpdate(arg0: () => void) {
     throw new Error("Method not implemented.");
   }
+  render() {
+    const boardConfigKeys = ["samples", "multiplicationFactor"];
+    for (const key of boardConfigKeys) {
+      const elt = querySelector(`div.command label.${key} span`);
+      console.log("elt: ", elt);
+      elt.innerHTML = this.config[key].toString();
+
+      const sliderElt = querySelector(`div.command label.${key} input`);
+      if (!(sliderElt instanceof HTMLInputElement)) {
+        throw new Error("SliderEltis not input elt");
+      }
+      sliderElt.value = this.config[key].toString();
+    }
+  }
   setConfig(config: BoardConfig) {
-    throw new Error("Method not implemented.");
+    this.config = config;
+    this.render();
   }
 }
