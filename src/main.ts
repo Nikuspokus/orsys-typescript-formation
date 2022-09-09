151d65b9dfe4cda5fe2491fc095b3ce4206de1d1
nmarquillier <nmarquillier@norauto.com>
2022-09-09 09:46:19 +0200

    dev: make many files

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,19 @@
+import {
+  computeAngle,
+  computePointCoordinate,
+  querySelector,
+} from "./utils.js";
+
 console.log("start");
 
-const computeAngle = (i, samples, multiplicationFactor?) => {
-  if (multiplicationFactor === undefined) {
-    multiplicationFactor = 1;
-  }
-  return i * ((2 * Math.PI) / samples) * multiplicationFactor - Math.PI / 2;
-};
-const computePointCoordinate = (angle) => {
-  const x = x0 + r0 * Math.cos(angle);
-  const y = y0 + r0 * Math.sin(angle);
-  return { x, y };
-};
-
-const querySelector = (cssSelector: string) => {
-  const elt = document.querySelector(cssSelector);
-  if (elt === null) {
-    throw new Error("Cannot find selector: " + cssSelector);
-  }
-  return elt;
-};
-
 const samples = 100;
-const multiplicationFactor = 2;
+const multiplicationFactor = 3;
 
 const svgns = "http://www.w3.org/2000/svg";
 
 const container = querySelector("svg g.samples");
 
 const lineContainer = querySelector("svg g.lines");
-
-const x0 = 50;
-const y0 = 50;
-const r0 = 45;
 
 const r = 1;
 
@@ -40,8 +22,7 @@ for (let i = 0; i < samples; i++) {
 
   const angle = computeAngle(i, samples);
 
-  const x = x0 + r0 * Math.cos(angle);
-  const y = y0 + r0 * Math.sin(angle);
+  const { x, y } = computePointCoordinate(angle);
 
   const circle = document.createElementNS(svgns, "circle");
   circle.setAttributeNS(null, "cx", `${x}`);
---
+++ src/utils.ts
@@ -0,0 +1,23 @@
+const x0 = 50;
+const y0 = 50;
+const r0 = 45;
+
+export const computeAngle = (i, samples, multiplicationFactor?) => {
+  if (multiplicationFactor === undefined) {
+    multiplicationFactor = 1;
+  }
+  return i * ((2 * Math.PI) / samples) * multiplicationFactor - Math.PI / 2;
+};
+export const computePointCoordinate = (angle) => {
+  const x = x0 + r0 * Math.cos(angle);
+  const y = y0 + r0 * Math.sin(angle);
+  return { x, y };
+};
+
+export const querySelector = (cssSelector: string) => {
+  const elt = document.querySelector(cssSelector);
+  if (elt === null) {
+    throw new Error("Cannot find selector: " + cssSelector);
+  }
+  return elt;
+};
